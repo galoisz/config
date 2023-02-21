021ae2ad52a498291466518402d8168ee38955e0
galoisz <galzfira@gmail.com>
2023-02-21 10:05:35 +0200

    config bind

---
+++ ConfigApp/ConfigApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConfigApp;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder().AddJsonFile($"appsettings.json");
+
+var config = configuration.Build();
+
+//var x = config.Get<custSettings>();
+var x = new custSettings();
+
+IConfigurationSection section = config.GetSection("Settings");
+
+section.Bind(x);
+
+Console.WriteLine(section.Value);
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
---
+++ ConfigApp/ConfigApp/custSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigApp
+{
+    public class custSettings
+    {
+        public string Position => "Settings";
+        public string KeyOne { get; set; }
+        public string KeyTwo { get; set; }
+    }
+}
